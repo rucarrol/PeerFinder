ca23c8ef81caaee75a984a03b2860f169c1176e3
Ruairi Carroll <ruairi.carroll@gmail.com>
2017-07-29 19:26:35 +0100

    - removed --myasn / --peerasn in favour of --asn - Fixed bug #2 and #3 which dealt with incorrect handling of --ix-only and --private-only - Refactored

--- peerfinder.py
+++ peerfinder.py
@@ -11,9 +11,133 @@ import argparse
 import requests
 from prettytable import PrettyTable
 
+
+def main():
+
+    args = getArgs()
+    asns = args.asn
+    pdata = dict() 
+    for asn in asns: 
+        pdata[asn] = getPeeringDB(asn)
+
+    # Lets check to see if we have valid data in our replies. 
+    try:
+        for asn in pdata.keys():
+            asn = pdata[str(asn)]['data'][0]['asn']
+            peer_name = pdata[str(asn)]['data'][0]['name']
+    except IndexError:
+        print("Looks like an empty dataset, exiting")
+        print("result: %s" % pdata[asn])
+        exit(1)
+
+    if args.ix_only:
+
+        # Dump all our ix names into a list
+        ixp = dict()
+        for asn in pdata.keys():
+            ixp[asn] = getFac(pdata[str(asn)], "netixlan_set")
+
+        # For all our IXs, see if they have the same IX
+        # Have to seed the common ix list with the first entry, otherwise the intersection of [] will always be []
+
+        common_ix_list = ixp[ixp.keys()[0]]
+        for asn in ixp:
+            common_ix_list = list(set(ixp[asn]).intersection(common_ix_list))
+
+
+        if len(common_ix_list) < 1:
+            print("Didnt find any common IX, exiting...")
+            exit(1)
+        header = []
+        header.append('IX')
+        for asn in pdata.keys():
+            header.append(pdata[asn]['data'][0]['name'])
+        ix_tab = PrettyTable(header)
+        ix_tab.print_empty = False
+
+        for ix in common_ix_list:
+            row = [ix]
+            for asn in pdata.keys():
+                for i in pdata[asn]['data'][0]['netixlan_set']:
+                    if i['name'] not in common_ix_list:
+                        continue
+                    if ix == i['name']:
+                        # Skip if ix is not in our shared list.
+                        for entry in row:
+                            if str(i['asn']) in entry:
+                                row.remove(entry)
+                                line = line + "\nv4: %s\nv6: %s" % (i['ipaddr4'],i['ipaddr6'])
+                            else: 
+                                line = "AS: %s\nv4: %s\nv6: %s" % (i['asn'],i['ipaddr4'],i['ipaddr6'])
+                        row.append(line)
+            ix_tab.add_row(row)
+
+        ix_tab.hrules = 1
+        print(ix_tab.get_string(sortby="IX"))
+
+
+    if args.fac_only:
+        # Dump all our ix names into a list
+        priv = dict()
+        for asn in pdata.keys():
+            priv[asn] = getFac(pdata[str(asn)], "netfac_set")
+
+        # For all our IXs, see if they have the same IX
+        # Have to seed the common ix list with the first entry, otherwise the intersection of [] will always be []
+
+        common_priv_list = priv[priv.keys()[0]]
+        for asn in priv:
+            common_priv_list = list(set(priv[asn]).intersection(common_priv_list))
+
+
+        if len(common_priv_list) < 1:
+            print("Didnt find any common Private facilities, exiting...")
+            exit(1)
+        header = ['Facility', 'City', 'Country']
+
+        fac_tab = PrettyTable(header)
+        fac_tab.print_empty = False
+
+        for fac in common_priv_list:
+            row = []
+            for asn in pdata.keys():
+                for i in pdata[asn]['data'][0]['netfac_set']:
+                    if i['name'] not in common_priv_list:
+                        continue
+                    if fac == i['name']:
+                        row = [i['name'], i['city'], i['country']]
+            fac_tab.add_row(row)
+
+        fac_tab.hrules = 1
+        print(fac_tab.get_string(sortby="Facility"))
+
+    exit(0)
+
+def getArgs():
+    help_text = "Generate a table for common points in an IX. -h for help"
+    parser = argparse.ArgumentParser(help_text)
+    parser.add_argument('--asn', nargs = '+', dest = 'asn', help = 'List of ASNs')
+
+    xgroup = parser.add_mutually_exclusive_group()
+    xgroup.add_argument('--ix-only',      dest='ix_only',  help='Print IX results only', action='store_true')
+    xgroup.add_argument('--private-only', dest='fac_only', help='Print private facility results only', action='store_true')
+
+    args = parser.parse_args()
+    ## Validate args here
+    return args
+
+
 def getPeeringDB(ASN):
+    """Function to connect to peeringDB and fetch results for a given ASN
+
+    Args:
+        ASN: A numeric, 32 bit number AS number.
+
+    Returns:
+        A dict which contains the dataset for a given ASN. Specific API docs here: https://api.peeringdb.com/apidocs/#!/net/Network_list
+
+    """
     HTTP_OK = 200
-    ## Get my data, their data
     pdb_url = 'https://api.peeringdb.com/api/net?asn__in=%s&depth=2' % ASN
     print("Fetching PeeringDB info for %s" % ASN)
     r = requests.get(pdb_url)
@@ -30,78 +154,5 @@ def getFac(pdb, nettype):
         fac_set.append(item['name'])
     return fac_set
 
-help_text = "Generate a table for common points in an IX. -h for help"
-parser = argparse.ArgumentParser(help_text)
-parser.add_argument('--myasn', help='My ASN', default='13414')
-parser.add_argument('--peerasn', help='Peer ASN', default='13414')
-
-xgroup = parser.add_mutually_exclusive_group()
-xgroup.add_argument('--ix-only', dest='ix_only', help='Print IX results only', action='store_false', default=True)
-xgroup.add_argument('--private-only', dest='fac_only',
-                    help='Print private facility results only', action='store_false', default=True)
-
-args = parser.parse_args()
-
-
-results_mine = getPeeringDB(args.myasn)
-results_theirs = getPeeringDB(args.peerasn)
-
-try:
-    asn = results_theirs['data'][0]['asn']
-    peer_name = results_theirs['data'][0]['name']
-except IndexError:
-    print("Looks like an empty dataset, exiting")
-    print("result: %s" % results_theirs)
-    exit(1)
-
-if not args.ix_only:
-
-    # Dump all our ix names into a list
-    my_ix_list = getFac(results_mine, "netixlan_set")
-    peer_ix_list = getFac(results_theirs, "netixlan_set")
-
-    # For all our IXs, see if they have the same IX
-    common_ix_list = list(set(my_ix_list).intersection(peer_ix_list))
-    
-    if len(common_ix_list) < 1 and not args.ix_only:
-        print("Didnt find any common IX")
-
-    ix_tab = PrettyTable(['IX', 'ASN', 'IPv4', 'IPv6'])
-    ix_tab .print_empty = False
-
-    for i in results_theirs['data'][0]['netixlan_set']:
-        # Skip if ix is not in our shared list.
-        ix = i['name']
-        if ix not in common_ix_list:
-            continue
-        # populate the dict : router->IX->Peering info
-        ix_tab.add_row([ix, i['asn'], i['ipaddr4'], i['ipaddr6']])
-
-    print(ix_tab.get_string(sortby="IX"))
-
-
-if not args.fac_only:
-    # Dump all our ix names into a list
-    my_priv_list = getFac(results_mine, "netfac_set")
-    peer_priv_list = getFac(results_theirs, "netfac_set")
-
-    # For all our IXs, see if they have the same IX
-    common_priv_list = list(set(my_priv_list).intersection(peer_priv_list))
-    if len(common_priv_list) < 1:
-        print("Didnt find any common private facilities")
-
-    # Find common facilities - only need city/country here.
-    priv_tab = PrettyTable(['Facility', 'City', 'Country'])
-    priv_tab.print_empty = False
-
-    for i in results_theirs['data'][0]['netfac_set']:
-        # Skip if ix is not in our shared list.
-        ix = i['name']
-        if ix not in common_priv_list:
-            continue
-        # populate the dict : router->IX->Peering info
-        priv_tab.add_row([ix, i['city'], i['country']])
-
-    print(priv_tab.get_string(sortby="Facility"))
-
-exit(0)
+if __name__ == "__main__":
+    main()
